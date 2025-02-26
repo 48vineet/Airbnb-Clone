b82d6a15de4819dc70f29549ce188fdf3075f5f5
Vineet Mali <7038vineet@gmail.com>
2025-02-26 23:48:26 +0530

    new files and updates

--- routes/listing.js
+++ routes/listing.js
@@ -32,6 +32,10 @@ router.get("/new", (req, res) => {
 router.get("/:id", warpAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id).populate("reviews");
+    if (!listing) {
+        req.flash("error", "Listing you requested for does not exist");
+        // res.redirect("/listings");
+    }
     res.render("listings/show.ejs", { listing });
 }));
 
@@ -42,6 +46,7 @@ router.post("/", validateListing,
 
         const newListing = new Listing(req.body.listing);
         await newListing.save();
+        req.flash("success", "New Listing Created!");
         res.redirect("/listings");
     })
 );
@@ -51,6 +56,10 @@ router.post("/", validateListing,
 router.get("/:id/edit", warpAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
+    if (!listing) {
+        req.flash("error", "Listing you requested for does not exist");
+        // res.redirect("/listings");
+    }
     res.render("listings/edit.ejs", { listing });
 }));
 
@@ -59,6 +68,7 @@ router.put("/:id/", validateListing, warpAsync(async (req, res) => {
 
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    req.flash("success", "Listing Updated Succesfully!");
     res.redirect(`/listings/${id}`);
 }));
 
@@ -68,6 +78,7 @@ router.delete("/:id", warpAsync(async (req, res) => {
     let { id } = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
+    req.flash("success", "Listing Deleted!");
     res.redirect("/listings");
 }));
 
